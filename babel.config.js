706a2970dd1db80e8247de29c6b2898cc8e42fbc
jamesshep64AM <jamesshep@tamu.edu>
2025-02-01 19:45:33 +0000

    gr

--- babel.config.js
+++ babel.config.js
@@ -60,12 +60,7 @@ module.exports = function(api) {
           loose: true
         }
       ],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false
-        }
-      ],
+      "@babel/plugin-proposal-private-methods",
       [
         '@babel/plugin-transform-regenerator',
         {
